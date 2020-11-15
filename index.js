263667e23025379bef3a946a8e913afa01baecd3
unknown <christopher.robertson@myport.ac.uk>
2020-11-15 17:52:09 +0000

    Manual Clock Tick, ROR via Interface

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var serialPort
 
 var evt = new EventEmitter()
 
-const BAUD_RATES = ["9600", "57600", "CUSTOM"]
+const BAUD_RATES = ["110", "300", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "CUSTOM"]
 const EEPROM_SIZE = 0x1fff;
 const EEPROM_LOCATION = 0xffff - EEPROM_SIZE
 
@@ -88,7 +88,7 @@ async function scanSerial() {
 
 	baudRate = +(await baudRateSelection())
 
-	var payload = Array.from(await askPayload());
+	var payload = Array.from(await askPayload() || []);
 
 	serialPort = new Serial(path, {
 		baudRate
@@ -103,18 +103,20 @@ async function scanSerial() {
 				console.log(`${payload.length} Bytes Remaining`);
 				if (payload.length) {
 
-					var buf = payload.splice(0,512)
+					var buf = payload.splice(0, 512)
 					buf[512] = "\n";
 					serialPort.write(buf)
 				} else {
-					//							writeToScreen("Done Uploading");
+					//writeToScreen("Done Uploading");
 					serialPort.write("Done Uploading");
 				}
 			} else if (str == 'Getting Size') {
 				console.log(Buffer.from([EEPROM_LOCATION & 0xff, EEPROM_LOCATION >> 8, 13]))
 				serialPort.write(Buffer.from([EEPROM_LOCATION & 0xff, EEPROM_LOCATION >> 8, 13]))
 			}
-			writeToScreen(str)
+			if (str.trim()) {
+				writeToScreen("<\t" + str)
+			}
 		}
 
 		const parser = serialPort.pipe(new Readline({
@@ -130,6 +132,19 @@ async function askPayload() {
 	return await response().then((r) => {
 		if (/^n$/i.test(r)) {} else {
 			return generatePayload()
+		}
+	})
+
+}
+
+async function askClock() {
+	writeToScreen('Free Running Clock? [Y/n]')
+
+	return await response().then((r) => {
+		if (/^n$/i.test(r)) {
+			return false
+		} else {
+			return true
 		}
 	})
 
@@ -155,8 +170,27 @@ async function generatePayload() {
 			return BAUD_RATES[f - 1]
 		} else if (f == binFiles.length + 1) {
 			var buf = Buffer.alloc(EEPROM_SIZE + 1, 0xea)
-			buf[EEPROM_SIZE - (0xffff - 0xfffc)] = (0xffff - EEPROM_SIZE) | 0x00;
+			buf[EEPROM_SIZE - (0xffff - 0xfffc)] = (0xffff - EEPROM_SIZE) | 0x01;
 			buf[EEPROM_SIZE - (0xffff - 0xfffd)] = (0xffff - EEPROM_SIZE) >> 8;
+
+			buf[1] = 0xa9;	//LDA#
+			buf[2] = 0xff; 
+			buf[3] = 0x8d;	//STA
+			buf[4] = 0x02;	//Register 0x02 DIR
+			buf[5] = 0xd0;
+			buf[6] = 0xa9;	//LDA#
+			buf[7] = 0x55;	//00000001
+			buf[8] = 0x8d;	//STA
+			buf[9] = 0x00;	//Register 0x00 OUTPUT
+			buf[10] = 0xd0;	
+			buf[11] = 0x6a;	//ROR
+			buf[12] = 0x8d;	//STA
+			buf[13] = 0x00;
+			buf[14] = 0xd0;	//Resister 0x00 OUTPUT
+			buf[15] = 0x4c;	//JMP
+			buf[16] = 0x0b;
+			buf[17] = 0xe0;
+
 			return buf
 		} else {
 			return generatePayload()
